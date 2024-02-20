569b683fa9fa7a82d63438c2c834db9e58ec0bdb
sckptt <142684484+sckptt@users.noreply.github.com>
2024-02-20 20:08:40 +0700

    Add files via upload
    WIP proklyaty ft split!!!!

--- Libft-wip/ft_split.c
+++ Libft-wip/ft_split.c
@@ -6,7 +6,7 @@
 /*   By: vkinsfat <vkinsfat@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/19 20:03:13 by vkinsfat          #+#    #+#             */
-/*   Updated: 2024/02/20 16:53:48 by vkinsfat         ###   ########.fr       */
+/*   Updated: 2024/02/20 20:00:30 by vkinsfat         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -43,19 +43,58 @@ int	count_words(char const *str, char s)
 	return (count);
 }
 
-void	fill_res(char *str, char s, char **res)
+int	count_letters(char const *str, char s)
 {
-	int i;
+	int	i;
+	int	count;
 
 	i = 0;
-	while (str[i] != '\0')
+	count = 0;
+	while (str[i] != s && str[i] != '\0')
 	{
-		if (str[i] != s)
-		{
-			*res = (char *)malloc(sizeof(char) * ());
-		}
+		count++;
+		i++;
 	}
-	
+	return (count);
+}
+
+void	fill_res(char const *str, char s, char **res)
+{
+	int i;
+	int j;
+	int len;
+
+	i = 0;
+	len = 0;
+	while (*str != '\0')
+	{
+		if (*str != s)
+		{
+			len = count_letters(str, s);
+			printf("%s\n", str);
+			res[i] = (char *)malloc(sizeof(char) * (len + 1));
+			if (!*res[i])
+			{
+				while (i > 0)
+				{
+					free(res[i]);
+					i--;
+				}
+			}
+			j = 0;
+			while (*str != s && *str != '\0')
+			{
+				res[i][j] = *str;
+				str++;
+				j++;
+			}
+			res[i][j] = '\0';
+			i++;
+		}
+		else
+			str++;
+	}
+	res[i] = NULL;
 }
 
 char	**ft_split(char const *s, char c)
@@ -74,22 +113,26 @@ char	**ft_split(char const *s, char c)
 	}
 	res = (char **)malloc(sizeof(char *) * (words + 1));
 	if (!res)
+	{
 		free(res);
 		return (NULL);
+	}
 	fill_res(s, c, res);
 	return (res);
 }
 
 int main()
 {
+	int i = 0;
 	char *s = " I   see a red door   and I want        to paint it black  ";
-	int words = count_words(s, ' ');
-	printf("%d\n", words);
+	// int words = count_words(s, ' ');
+	// printf("%d\n", words);
 	char **res = ft_split(s, ' ');
-	while (**res)
+	while (*res[i])
 	{
-		printf("%s\n", *res);
-		*res++;
+		printf("%d\n", i);
+		printf("%s\n", res[i]);
+		i++;
 	}
 	return 0;
 }
