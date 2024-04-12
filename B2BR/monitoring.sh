5282d19f7e25a1fe8d9dd2217efff94e9f0cffe8
sckptt <142684484+sckptt@users.noreply.github.com>
2024-04-12 20:36:00 +0700

    Final version

--- B2BR/monitoring.sh
+++ B2BR/monitoring.sh
@@ -1,39 +1,53 @@
 #!/bin/bash
 
 # Architecture shows here:
-echo -n "#Architecture: "; uname -a
+arch=$(uname -a)
 
 # Number of physical processors shows here:
-echo -n "#CPU physical :"; grep "physical id" /proc/cpuinfo | wc -l
+cpuph=$(grep "physical id" /proc/cpuinfo | wc -l)
 
 # Number of virtual processors shows here:
-echo -n "#vCPU : "; grep processor /proc/cpuinfo | wc -l
+vcpu=$(grep processor /proc/cpuinfo | wc -l)
 
 # RAM usage info shows here:
-echo -n "#Memory Usage: "; free -m | awk '$1 == "Mem:" {printf "%s/%sMb (%.2f%%)\n", $3, $2, $3/$2*100}'
+memus=$(free -m | awk '$1 == "Mem:" {printf "%s/%sMb (%.2f%%)\n", $3, $2, $3/$2*100}')
 
 # Disk memory usage info shows here:
-echo -n "#Disk Usage: "; df -m | grep "/dev/" | gerp -v "/boot" | awk '{memory_used += $3} {memory_total += $2} END {printf "%s/%.0fGb (%.d%%)\n", memory_used, memory_total/1024, memory_used/memory_total*100}'
+diskus=$(df -m | grep "/dev/" | grep -v "/boot" | awk '{memory_used += $3} {memory_total += $2} END {printf "%s/%.0fGb (%.d%%)\n", memory_used, memory_total/1024, memory_used/memory_total*100}')
 
 # CPU load shows here:
-echo -n "#CPU load: "; vmstat | tall -1 | awk '{print $15}'
+cpul=$(top -bn1 | grep -A 1 PID | tail -n 1 | awk '{print $9}')
 
 # Last reboot info shows here:
-echo -n "#Last boot: "; who -b | awk '$1 == "system" {print $3 " " $4}'
+lb=$(who -b | awk '$1 == "system" {print $3 " " $4}')
 
 # LVM use info shows here:
-echo -n "#LVM use: "; lsblk | grep lvm | awk '{if ($1) {print "yes"; exit;} else {print "no"}}'
+lvmus=$(lsblk | grep lvm | awk '{if ($1) {print "yes"; exit;} else {print "no"}}')
 
 # The number of TCP connections shows here:
-echo -n "#Connections TCP : "; ss -ta | grep ESTAB | wc -l
+ctcp=$(ss -ta | grep ESTAB | wc -l)
 
 # The number of users using the server shows here:
-echo -n "#User log: "; users | wc -l
+uslog=$(users | wc -l)
 
 # The IPv4 address of your server and its 
 # MAC (Media Access Control) address shows here:
-echo -n "#Network: "; ip link | grep "link/ether" | awk '{print $2}'
+ip=$(hostname -I)
+nw=$(ip link | grep "link/ether" | awk '{print $2}')
 
 # The number of commands executed with 
 # the sudo program shows here:
-echo -n "#Sudo : "; journalctl _COMM=sudo | grep COMMAND | wc -l
+exsudo=$(journalctl _COMM=sudo | grep COMMAND | wc -l)
+
+wall " #Architecture: $arch
+#CPU physical: $cpuph
+#vCPU: $vcpu
+#Memory Usage: $memus
+#Disk Usage: $diskus
+#CPU load: $cpul%
+#Last boot: $lb
+#LVM use: $lvmus
+#Connections TCP: $ctcp ESTABLISED
+#User log: $uslog
+#Network: IP $ip ($nw)
+#Sudo: $exsudo cmd"
