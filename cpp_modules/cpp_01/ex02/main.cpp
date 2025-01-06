b62a74a12a712279adfa0f0f4dd42be4c9f681d3
sckptt <kiddovpm@gmail.com>
2025-01-06 14:01:01 +0700

    cpp_01_02 is done

--- cpp_modules/cpp_01/ex02/main.cpp
+++ cpp_modules/cpp_01/ex02/main.cpp
@@ -6,11 +6,26 @@
 /*   By: vitakinsfator <vitakinsfator@student.42    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/05 15:35:16 by vitakinsfat       #+#    #+#             */
-/*   Updated: 2025/01/05 15:37:45 by vitakinsfat      ###   ########.fr       */
+/*   Updated: 2025/01/06 13:36:14 by vitakinsfat      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <string>
+#include <iostream>
+
 int main(void)
 {
-	
+	std::string string = "HI THIS IS BRAIN";
+	std::string* stringPTR = &string;
+	std::string& stringREF = string;
+
+	std::cout << "Printing memory addresses:\n";
+	std::cout << &string << std::endl;
+	std::cout << stringPTR << std::endl;
+	std::cout << &stringREF << "\n" << std::endl;
+
+	std::cout << "Printing values:\n";
+	std::cout << string << std::endl;
+	std::cout << *stringPTR << std::endl;
+	std::cout << stringREF << std::endl;
 }
